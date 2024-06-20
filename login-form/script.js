ae30c41e3dc51b69d2cd3077c24d08627b4c7f9d
Varun Arora <varunarora137@gmail.com>
2024-06-20 18:12:11 +0530

    final commit

--- login-form/script.js
+++ login-form/script.js
@@ -104,14 +104,3 @@ signup.addEventListener("click", (e) => {
     container.classList.remove("sign-up-mode2");
   }, 2000);
 });
-
-// const sign_in_btn = document.querySelector("#sign-in-btn");
-// const sign_up_btn = document.querySelector("#sign-up-btn");
-// const container = document.querySelector(".container");
-
-// sign_up_btn.addEventListener("click", () => {
-//   container.classList.add("sign-up-mode");
-// });
-// sign_in_btn.addEventListener("click", () => {
-//   container.classList.remove("sign-up-mode");
-// });
